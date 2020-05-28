0d85710ba9b65ce551b633bffe80f05812f00c86
yossiee <38056766+yossiee@users.noreply.github.com>
2020-05-28 22:50:58 +0900

    Fix: tweet text
    because tweet the same text will occur 403 error

--- post/tweet_test.go
+++ post/tweet_test.go
@@ -2,6 +2,7 @@ package post
 
 import (
 	"testing"
+	"time"
 
 	. "github.com/yossiee/97programmerbot/auth"
 )
@@ -11,7 +12,7 @@ const env = "../%s.env"
 func TestPostTweetSuccess(t *testing.T) {
 	a, _ := Auth(env)
 
-	err := PostTweet(*a, "Test tweet", nil)
+	err := PostTweet(*a, time.Now().Format("2006年01月02日 15時04分05秒"), nil)
 	if err != nil {
 		t.Errorf("\nexpect: nil.\nactual: %#v.\n", err.Error())
 	}
